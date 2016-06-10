e229c565e0bcce968e728b35bec8ae28ff8501f1
stormning <stormning@163.com>
2016-06-10 23:26:37 +0800

    fix spelling mistake

--- src/main/java/com/slyak/spring/JpaExtraAutoConfiguration.java
+++ src/main/java/com/slyak/spring/JpaExtraAutoConfiguration.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 @EnableConfigurationProperties(SpringJpaProperties.class)
-public class JpaExtraAutoConfigration {
+public class JpaExtraAutoConfiguration {
 
     @Autowired
     private SpringJpaProperties springJpaProperties;
