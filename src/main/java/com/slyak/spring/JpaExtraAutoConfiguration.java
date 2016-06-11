25ad84ff2d0b6d0b0ca3f85f7e178c5f5bb09efe
stormning <stormning@163.com>
2016-06-11 10:32:43 +0800

    add missing dependency

--- src/main/java/com/slyak/spring/JpaExtraAutoConfiguration.java
+++ src/main/java/com/slyak/spring/JpaExtraAutoConfiguration.java
@@ -2,6 +2,8 @@ package com.slyak.spring;
 
 import com.slyak.spring.jpa.FreemarkerSqlTemplates;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +15,7 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 @EnableConfigurationProperties(SpringJpaExtraProperties.class)
+@AutoConfigureAfter({ DataSourceAutoConfiguration.class })
 public class JpaExtraAutoConfiguration {
 
     @Autowired
